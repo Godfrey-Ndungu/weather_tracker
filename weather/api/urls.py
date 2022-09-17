1e828018c0b564f58a0b610708069240211b7950
Godfrey <godmak2022@gmail.com>
2022-09-18 00:17:53 +0300

    Added django basic migrations

--- weather/api/urls.py
+++ weather/api/urls.py
@@ -4,5 +4,5 @@ from .views import WeatherViewSet
 app_name='api'
 
 urlpatterns = [
-        path('/api/locations/<pk>/',WeatherViewSet.as_view({'get': 'retrieve'}),name="weather_detail")
+        path('api/locations/<pk>/',WeatherViewSet.as_view({'get': 'retrieve'}),name="weather_detail")
 ]
