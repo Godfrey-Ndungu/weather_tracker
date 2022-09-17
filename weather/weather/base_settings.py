28e707fea49caa9dc0c7987a4ff7863680ca3676
Godfrey <godmak2022@gmail.com>
2022-09-17 23:51:17 +0300

    Added api app

--- weather/weather/base_settings.py
+++ weather/weather/base_settings.py
@@ -16,7 +16,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps
+    'api',
 
+    # 3rd party
     'rest_framework',
 ]
 
