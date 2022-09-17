3d1dad15b44f0adc1cc7b9e39d67840afd69e081
Godfrey <godmak2022@gmail.com>
2022-09-17 22:46:17 +0300

    Add database configurations for deployment

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -1,1 +1,12 @@
 from .base_settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':config('NAME'),
+        'USER':config('NAME'),
+        'PASSWORD':config('PASSWORD'),
+        'HOST':config('HOST'),
+        'PORT': '',
+    }
+}
