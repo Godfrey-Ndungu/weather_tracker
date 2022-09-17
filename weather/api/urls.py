9064e498e9a6b48d24997848f294579cb69d2093
Godfrey <godmak2022@gmail.com>
2022-09-18 00:10:40 +0300

    Updated api url for weather viewset

--- weather/api/urls.py
+++ weather/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+
+from .views import WeatherViewSet
 app_name='api'
 
 urlpatterns = [
-    
+        path('/api/locations/<pk>/',WeatherViewSet.as_view({'get': 'retrieve'}),name="weather_detail")
 ]
