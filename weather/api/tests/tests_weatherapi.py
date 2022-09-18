bba3e359c1c1e1d79a510fabf6e972a8ed757095
Godfrey <godmak2022@gmail.com>
2022-09-18 18:51:25 +0300

    Added edge values for weather api tests

--- weather/api/tests/tests_weatherapi.py
+++ weather/api/tests/tests_weatherapi.py
@@ -6,19 +6,32 @@ from faker import Faker
 fake = Faker()
 
 class WeatherAPITestCase(TestCase):
-    def setUp(self):
-        pass
-
-    def test_init_method_variables_are_none(self):
-        api = WeatherAPI()
-        self.assertEqual(api.api, None)
-        self.assertEqual(api.api_key, None)
-        
-    def test_init_method_variables_are_none(self):
+    def test_variables_returned_by_init_method(self):
         city=fake.text()
         days=12
         api = WeatherAPI(city,days)
         self.assertEqual(api.api, city)
         self.assertEqual(api.api_key, days)
 
+    def test_city_weather_forecast_for_specific_days_method(self):
+        api_class = WeatherAPI()
+        api_method= api_class._get_city_weather_forecast_for_specific_days('london',12)
+        self.assertEqual(api_method[0],200)
+
+    def test_city_weather_forecast_for_city_not_existing_method(self):
+        api_class = WeatherAPI()
+        api_method= api_class._get_city_weather_forecast_for_specific_days('citynotavailable12',12)
+        self.assertNotEqual(api_method[0],200)
+    
+    def test_city_weather_forecast_number_of_days_not_integer(self):
+        # if number of days not provided will use 0 as default
+        api_class = WeatherAPI()
+        api_method= api_class._get_city_weather_forecast_for_specific_days('london','number_of_days_string')
+        self.assertEqual(api_method[0],200)
+    
+    def test_city_weather_forecast_number_of_days_not_integera_and_city_integer(self):
+        api_class = WeatherAPI()
+        api_method= api_class._get_city_weather_forecast_for_specific_days(12,'number_of_days_string')
+        self.assertNotEqual(api_method[0],200)
+    
     
